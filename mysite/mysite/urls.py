f2f1dd3dd5cf5edb1ebfd534ac32288226230260
AndrewMori <andrewmori@backcode.com>
2013-08-02 16:01:35 -0700

    tests/voting/generic views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
     # ex: /polls/
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
